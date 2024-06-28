0061a7c2736de1efba787306264f7727ce5a7da2
Or Sahar <osahar@paloaltonetworks.com>
2024-06-28 22:42:57 +0300

    interfaces

--- src/handlers/event_handler.go
+++ src/handlers/event_handler.go
@@ -6,8 +6,6 @@ import (
 )
 
 // Handle logs the event to the console.
-
-// Handle logs the event to the console.
 func Handle(event interfaces.Event) {
 	log.Printf("Event received - Key: %s, Value: %s\n", event.Key(), event.Value())
 }
